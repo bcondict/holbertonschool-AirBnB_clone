95852279bfd81bf683c0696a299d6c4b32890326
Jesus <jesus.junc10@gmail.com>
2022-06-30 15:43:37 -0500

    base_model update

--- models/base_model.py
+++ models/base_model.py
@@ -7,35 +7,21 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    """"""
+    def __init__(self, *args, **kwargs):
         """ """
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.now())
-        self.updated_at = str(datetime.now())
 
-    
-    def created_at_method(self):
-        """
-            assign with the  currente datetime when an instance is created
-        """
-        created_at = str(datetime.now())
-        print(created_at.isoformat())
-
-    def updated_at_method(self):
-        """
-            assign twith the current datetime when an instance is created
-            and it will be updated every time you change your object
-        """
-        updated_at = str(datetime.now())
-        print(updated_at.isoformat())
-    
+        self.created_at = str(datetime.now().isoformat())
+        self.updated_at = str(datetime.now().isoformat())
 
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
         my_dict = self.__dict__
+        my_dict["__class__"] = self.__class__.__name__
         return my_dict
