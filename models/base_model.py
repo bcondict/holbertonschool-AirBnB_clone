45d4660aa6774255a704ceb7d0d4897386533a82
DanielN24 <capdaniel_98@outlook.com>
2022-07-03 22:44:46 -0500

    nome changes in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -4,38 +4,35 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    def __init__(self):
+    """"""
+    def __init__(self, *args, **kwargs):
         """ """
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.now())
-        self.updated_at = str(datetime.now())
-
-    
-    def created_at_method(self):
-        """
-            assign with the  currente datetime when an instance is created
-        """
-        created_at = str(datetime.now())
-        print(created_at.isoformat())
-
-    def updated_at_method(self):
-        """
-            assign twith the current datetime when an instance is created
-            and it will be updated every time you change your object
-        """
-        updated_at = str(datetime.now())
-        print(updated_at.isoformat())
-    
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        my_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if bool(kwargs):
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, my_format)
+                else:
+                    self.__dict__[key] = value
+        storage.new()
 
     def __str__(self):
         return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         my_dict = self.__dict__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
         return my_dict
