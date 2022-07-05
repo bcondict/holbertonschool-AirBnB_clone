512af749eaef55ba4c09e4366d6d39a9a78cf70e
DanielN24 <capdaniel_98@outlook.com>
2022-07-05 18:52:39 -0500

    new files

--- console.py
+++ console.py
@@ -2,8 +2,19 @@
 """ """
 
 
-import cmd, sys
+import cmd, sys, shlex, models
 from models.base_model import BaseModel
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+
+classes = {"BaseModel" : BaseModel, "User" : User, "State" : State, "City" : City,
+            "Amenity" : Amenity, "Place" : Place, "Review" : Review}
+
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,24 +30,58 @@ class HBNBCommand(cmd.Cmd):
         """Emptyline print a new line"""
         print()
         pass
-    def do_create(self, arg):
-        line = arg.split()
-        if len(line == 0)
-            print("** class name missing **")
-        if line[0] not "BaseModel":
-            print("** class doesn't exist **")
-        
 
+    def do_create(self, arg):
+        """  """
+        line = arg.split()
+        if len(line) == 0:
+            print("** class name missing **")
+        if line[0] != "BaseModel":
+            print("** class doesn't exist **")
+        instance = classes[line[0]]()
+        print(instance.id)
+        instance.save()
 
     def do_show(self, arg):
-        pass
+        """  """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    print(models.storage.all()[key])
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
     def do_destroy(self, arg):
-        pass
+        """  """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + "." + args[1]
+                if key in models.storage.all():
+                    models.storage.all().pop(key)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
+
     def do_all(self, arg):
         pass
     def do_update(self, arg):
-        pass
-    def do_show(self, arg):
         pass
 
 if __name__ == '__main__':
